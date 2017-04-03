020d94f624c826aa5e90e3449dd99b6386e15a80
Mariano Guerra <mariano@marianoguerra.org>
2017-04-03 15:35:47 +0200

    update copyright year

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Little Riak Core Book'
-copyright = u'2015, Mariano Guerra'
+copyright = u'2017, Mariano Guerra'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
